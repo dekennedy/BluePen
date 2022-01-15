ba4b9451c886e22535b7dadd5c9842a381674d28
David Kennedy <davidedgarkennedy@gmail.com>
2022-01-15 08:56:28 -0700

    Sub Plots

--- Week3_Plots.py
+++ Week3_Plots.py
@@ -1,7 +1,36 @@
 #%%
 
+#imports
 
-print('hello')
+import imp
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+
+plt.figure()
+
+# subplot with 1 row, 2 columns, and current axis is 1st subplot axes
+plt.subplot(1, 2, 1)
+
+linear_data = np.array([1,2,3,4,5,6,7,8])
+
+plt.plot(linear_data, '-o')
+
+#define Exponential_data
+exponential_data = linear_data**2
+
+#Rows, Columns, Plot number
+
+# Plot number 2
+plt.subplot(1,2,2)
+plt.plot(exponential_data, '-o')
+
+
+
+
+
 
 
 # %%
