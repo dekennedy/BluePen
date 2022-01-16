8dd61532dc79bc1b513498ab51d80b59b55cc87b
David Kennedy <davidedgarkennedy@gmail.com>
2022-01-16 08:40:03 -0700

    box plot

--- Week3_Plots.py
+++ Week3_Plots.py
@@ -67,3 +67,44 @@ for n in range(0,len(axs)):
 
 
 # %%
+
+##Box plot
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+normal_sample = np.random.normal(loc=0.0, scale=1.0, size=10000)
+random_sample = np.random.random(size=10000)
+gamma_sample = np.random.gamma(2, size=10000)
+
+df = pd.DataFrame({'normal': normal_sample, 
+                   'random': random_sample, 
+                   'gamma': gamma_sample})
+
+
+
+df.describe()                  
+
+
+
+
+
+# %%
+
+plt.figure()
+# create a boxplot of the normal data, assign the output to a variable to supress output
+_ = plt.boxplot(df['normal'])
+
+
+
+# %%
+
+plt.figure()
+plt.hist(df['gamma'], bins=100)
+
+plt.show
+
+# %%
