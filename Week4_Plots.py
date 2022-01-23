94d456afeba1402af2ef18c7d1f2e7be19c684ed
David Kennedy <davidedgarkennedy@gmail.com>
2022-01-23 08:34:37 -0700

    Plots

--- Week4_Plots.py
+++ Week4_Plots.py
@@ -7,3 +7,19 @@ import matplotlib.pyplot as plt
 
 
 # %%
+# use the 'seaborn-colorblind' style
+plt.style.use('seaborn-colorblind')
+# %%
+np.random.seed(123)
+
+df = pd.DataFrame({'A': np.random.randn(365).cumsum(0), 
+                   'B': np.random.randn(365).cumsum(0) + 20,
+                   'C': np.random.randn(365).cumsum(0) - 20}, 
+                  index=pd.date_range('1/1/2017', periods=365))
+df.head()
+# %%
+df.plot(); # add a semi-colon to the end of the plotting call to suppress unwanted output
+# %%
+# create a scatter plot of columns 'A' and 'C', with changing color (c) and size (s) based on column 'B'
+df.plot.scatter('A', 'C', c='B', s=df['B'], colormap='viridis')
+# %%
