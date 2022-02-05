d0d8d5a164977e65398c84b263f9de9b53fd017e
David Kennedy <davidedgarkennedy@gmail.com>
2022-02-05 08:06:49 -0700

    imports pip install

--- ML Week_1.py
+++ ML Week_1.py
@@ -1,6 +1,15 @@
-%matplotlib notebook
+
+# %matplotlib notebook
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
+#%%
+
+
+print('Done')
+# %%
