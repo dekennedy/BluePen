676a787785189da282befe8b3e2574b9bf5bc5fc
David Kennedy <davidedgarkennedy@gmail.com>
2022-02-06 08:49:28 -0700

    Train test Split

--- ML Week_1.py
+++ ML Week_1.py
@@ -7,8 +7,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
 #%%
+
+X_train, X_test, y_train, y_test = train_test_split(X_fruits, y_fruits, random_state=0)
+
+
 
 
 print('Done')
