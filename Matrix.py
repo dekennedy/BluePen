71192b8df9caa9e7131ba40a5c03a02ef0821dfb
David Kennedy <davidedgarkennedy@gmail.com>
2022-03-20 08:22:54 -0600

    100%

--- Matrix.py
+++ Matrix.py
@@ -1,11 +1,11 @@
 
 import numpy as np
 
-T = np.array([[2, 7],
-              [0, -1]])
+T = np.array([[1, 0],
+              [0, 1]])
 
-C = np.array([[7, 1],
-             [-3, 0]])
+C = np.array([[1, 2],
+             [0, 1]])
 
 
 Cinv = np.linalg.inv(C)
@@ -13,8 +13,14 @@ Cinv = np.linalg.inv(C)
 # print(T.diagonal())
 # print(Cinv)
 
-D = Cinv@T@C
+# D = Cinv@T@C
+# D = np.round(D , decimals = 2)
+# print((D))
+
+
+# print(T@T@T@T@T)
+
+
+D = C@T@Cinv
 D = np.round(D , decimals = 2)
 print((D))
-
-
